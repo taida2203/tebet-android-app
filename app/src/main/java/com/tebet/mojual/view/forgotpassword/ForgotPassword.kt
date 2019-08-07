fa0dfa3ccb8dfdeba69a17fcf1e90ddbfc467ea1
Tai Dao <taida2203@gmail.com>
2019-08-07 18:08:35 +0700

    fix(forgotpass): show top right logo

--- app/src/main/java/com/tebet/mojual/view/forgotpassword/ForgotPassword.kt
+++ app/src/main/java/com/tebet/mojual/view/forgotpassword/ForgotPassword.kt
@@ -2,11 +2,13 @@ package com.tebet.mojual.view.forgotpassword
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import br.com.ilhasoft.support.validation.Validator
 import com.tebet.mojual.BR
 import com.tebet.mojual.R
 import com.tebet.mojual.databinding.ActivitySignUpPasswordBinding
+import com.tebet.mojual.databinding.ItemHomeIconBinding
 import com.tebet.mojual.view.home.HomeActivity
 import com.tebet.mojual.view.base.BaseActivity
 
@@ -22,12 +24,15 @@ open class ForgotPassword : BaseActivity<ActivitySignUpPasswordBinding, ForgotPa
         get() = R.layout.activity_sign_up_password
 
     private lateinit var validator: Validator
+    private var topRightViewBinding: ItemHomeIconBinding? = null
 
     override fun onCreateBase(savedInstanceState: Bundle?, layoutId: Int) {
         viewModel.navigator = this
         enableBackButton = false
         validator = Validator(viewDataBinding)
         title = "Input new password"
+        topRightViewBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.item_home_icon, baseBinding.topRightHolder, true)
     }
 
     override fun openHomeScreen() {
