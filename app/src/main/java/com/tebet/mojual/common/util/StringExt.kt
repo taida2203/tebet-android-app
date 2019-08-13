944c9c57e55db56110518c6a02d023c586aeb900
Tai Dao <taida2203@gmail.com>
2019-08-13 16:54:06 +0700

    feat(future-sale-detail): add dynamic dot for currency

--- app/src/main/java/com/tebet/mojual/common/util/StringExt.kt
+++ app/src/main/java/com/tebet/mojual/common/util/StringExt.kt
@@ -1,8 +1,12 @@
 package com.tebet.mojual.common.util
 
+import java.text.DecimalFormat
+
 fun Double.toDisplayMoney(): String {
     if (this <= 0) {
-        return "Rp. 0"
+        return "Rp 0"
     }
-    return "Rp. ${this.toInt()}"
+    val formatter = DecimalFormat("#,###")
+    val formattedNumber = formatter.format(this)
+    return "Rp $formattedNumber"
 }
