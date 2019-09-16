86f3d4219fe244b91f919b2087b465ac9c4614f7
Tai Dao <taida2203@gmail.com>
2019-09-16 14:12:49 +0700

    fix(inbox): parsing date

--- app/src/main/java/com/tebet/mojual/common/util/DateExt.kt
+++ app/src/main/java/com/tebet/mojual/common/util/DateExt.kt
@@ -3,6 +3,17 @@ package com.tebet.mojual.common.util
 import java.text.SimpleDateFormat
 import java.util.*
 
+fun Long.toDisplayFullDate(): String {
+    if (this <= 0) {
+        return ""
+    }
+    val formatter = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
+    // Create a calendar object that will convert the date and time value in milliseconds to date.
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this
+    return formatter.format(calendar.time)
+}
+
 fun Long.toDisplayDate(): String {
     if (this <= 0) {
         return ""
