8c8fdc44aa0f7d73117778592e4e66f6f94ab2fe
Tai Dao <taida2203@gmail.com>
2019-09-17 18:07:43 +0700

    feat(notification): show yesterday for date

--- app/src/main/java/com/tebet/mojual/common/util/DateExt.kt
+++ app/src/main/java/com/tebet/mojual/common/util/DateExt.kt
@@ -1,5 +1,7 @@
 package com.tebet.mojual.common.util
 
+import android.text.format.DateFormat
+import com.tebet.mojual.R
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -7,11 +9,29 @@ fun Long.toDisplayFullDate(): String {
     if (this <= 0) {
         return ""
     }
-    val formatter = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
-    // Create a calendar object that will convert the date and time value in milliseconds to date.
-    val calendar = Calendar.getInstance()
-    calendar.timeInMillis = this
-    return formatter.format(calendar.time)
+
+    val smsTime = Calendar.getInstance()
+    smsTime.timeInMillis = this
+
+    val now = Calendar.getInstance()
+
+    val timeFormatString = "h:mm aa"
+    val dateTimeFormatString = "dd MMM, yyyy"
+    return if (now.get(Calendar.DATE) == smsTime.get(Calendar.DATE)) {
+        Utility.getInstance().getString(R.string.message_today) + " " + DateFormat.format(
+            timeFormatString,
+            smsTime
+        )
+    } else if (now.get(Calendar.DATE) - smsTime.get(Calendar.DATE) == 1) {
+        Utility.getInstance().getString(R.string.message_yesterday) + " " + DateFormat.format(
+            timeFormatString,
+            smsTime
+        )
+    } else if (now.get(Calendar.YEAR) == smsTime.get(Calendar.YEAR)) {
+        DateFormat.format(dateTimeFormatString, smsTime).toString()
+    } else {
+        DateFormat.format("dd MMM, yyyy", smsTime).toString()
+    }
 }
 
 fun Long.toDisplayDate(): String {
