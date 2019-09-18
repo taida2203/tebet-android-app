e172e5533b7a674f23e0ade25764e6490aa89e28
Tai Dao <taida2203@gmail.com>
2019-09-18 13:28:53 +0700

    fix(add-container): container title not refresh when changed

--- app/src/main/java/com/tebet/mojual/data/models/Quality.kt
+++ app/src/main/java/com/tebet/mojual/data/models/Quality.kt
@@ -17,18 +17,23 @@ data class Quality(
     var orderId: Long = -1,
     var orderCode: String = "",
     var assetId: Long? = null,
-    var combinedCode: String? = "",
     var sensorData: String? = null
 ) : Serializable, BaseObservable() {
     var assetCode: String = ""
-        @Bindable get() = field
+        @Bindable get
         set(value) {
             field = value
             notifyPropertyChanged(BR.assetCode)
         }
+    var combinedCode: String? = ""
+        @Bindable get
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.combinedCode)
+        }
 
     var weight: Double = 20.0
-        @Bindable get() = field
+        @Bindable get
         set(value) {
             field = value
             notifyPropertyChanged(BR.weight)
@@ -36,7 +41,7 @@ data class Quality(
 
     @Ignore
     var isSelected: Boolean = false
-        @Bindable get() = field
+        @Bindable get
         set(value) {
             field = value
             notifyPropertyChanged(BR.selected)
