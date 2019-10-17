79cbe2a570af2bdd46a2922630f6b95e6e903d6b
Tai Dao <taida2203@gmail.com>
2019-10-17 18:24:55 +0700

    feat(registration): intergration firebase flow for registration

--- auth/src/main/java/co/sdk/auth/core/AuthGooglePhoneLoginMethod.kt
+++ auth/src/main/java/co/sdk/auth/core/AuthGooglePhoneLoginMethod.kt
@@ -6,7 +6,10 @@ import android.content.Intent
 import co.sdk.auth.core.models.ApiCallBack
 import co.sdk.auth.core.models.LoginConfiguration
 import co.sdk.auth.core.models.LoginException
+import co.sdk.auth.view.InputVerifyCodeFragment
 import co.sdk.auth.view.LoginOTPActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
 
 class AuthGooglePhoneLoginMethod : AuthMethod {
     private var callback: ApiCallBack<LoginConfiguration>? = null
@@ -14,7 +17,7 @@ class AuthGooglePhoneLoginMethod : AuthMethod {
 
     val isLoggedIn: Boolean
         get() {
-            return false
+            return FirebaseAuth.getInstance().currentUser != null
         }
 
     override fun brandedLogin(
@@ -25,61 +28,26 @@ class AuthGooglePhoneLoginMethod : AuthMethod {
         this.callback = callback
         this.configuration = configuration
         if (isLoggedIn) {
-            // Handle Returning User
-//            if (AccountKit.getCurrentAccessToken() != null) {
-//                configuration.token = AccountKit.getCurrentAccessToken()?.token
-//            }
-//            AccountKit.getCurrentAccount(object : AccountKitCallback<Account> {
-//                override fun onSuccess(account: Account) {
-//                    val phoneNumber = account.phoneNumber
-//                    val phoneNumberString = phoneNumber.toString()
-//                    try {
-//                        configuration.phone = phoneNumberString.replace("\\+".toRegex(), "")
-//                            .replace(" ".toRegex(), "")
-//                            .trim { it <= ' ' }
-//                    } catch (ignored: Exception) {
-//                        Timber.e(ignored)
-//                    }
-//
-//                    configuration.token = AccountKit.getCurrentAccessToken()?.token
-//                    configuration.let { callback.onSuccess(200, it) }
-//                }
-//
-//                override fun onError(error: AccountKitError) {
-//                    callback.onFailed(LoginException(400, error.userFacingMessage))
-//                }
-//            })
+            if (FirebaseAuth.getInstance().currentUser != null) {
+                configuration.phone = FirebaseAuth.getInstance().currentUser?.phoneNumber
+                FirebaseAuth.getInstance().currentUser!!.getIdToken(true)
+                    .addOnSuccessListener { task ->
+                        configuration.token = task.token
+                        configuration.let { callback.onSuccess(200, it) }
+                    }.addOnFailureListener {
+                        configuration.let { callback.onFailed(LoginException(400, "")) }
+                    }
+            } else {
+                configuration.let { callback.onSuccess(500, it) }
+            }
         } else {
             val intent = Intent(context, LoginOTPActivity::class.java)
-//
-//            //            String language = Locale.getDefault().toString();
-//            var countryCode = "US"
-//            //            if (!TextUtils.isEmpty(language) && !"in-ID".equalsIgnoreCase(language) && !"in_ID".equalsIgnoreCase(language)) {
-//            //                countryCode = "US";
-//            //            } else {
-//            //                countryCode = "ID";
-//            //            }
-//            countryCode = "ID"
-//
-//            val configurationBuilder = com.facebook.accountkit.ui.AccountKitConfiguration.AccountKitConfigurationBuilder(
-//                    com.facebook.accountkit.ui.LoginType.PHONE,
-//                    AccountKitActivity.ResponseType.TOKEN)
-//                    .setDefaultCountryCode(countryCode)
-//                    .setFacebookNotificationsEnabled(true)
-//            if (!TextUtils.isEmpty(configuration.phone)) {
-//                val phone = configuration.phone?.let { PhoneNumber(null!!, it, null) }
-//                configurationBuilder.setInitialPhoneNumber(phone)
-//            }
-//            // ... perform additional configuration ...
-//            intent.putExtra(
-//                    AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-//                    configurationBuilder.build())
             context.startActivityForResult(intent, REQUEST_CODE_LOGIN_FIRE_BASE)
         }
     }
 
     override fun logout(context: Context, forceLogout: Boolean, callback: ApiCallBack<*>?) {
-//        AccountKit.logOut()
+        FirebaseAuth.getInstance().signOut()
         callback?.onSuccess(200, null)
     }
 
