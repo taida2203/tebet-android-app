541381babea9953a4d10ef2fe6638a1d7f77844f
Tai Dao <taida2203@gmail.com>
2019-10-25 16:04:48 +0700

    fix(app): translate order status to bahasa

--- app/src/main/java/com/tebet/mojual/common/util/StringExt.kt
+++ app/src/main/java/com/tebet/mojual/common/util/StringExt.kt
@@ -1,9 +1,11 @@
 package com.tebet.mojual.common.util
 
 import com.tebet.mojual.R
+import com.tebet.mojual.data.models.enumeration.ContainerOrderState
 import com.tebet.mojual.data.models.enumeration.OrderStatus
 import com.tebet.mojual.data.models.enumeration.SortBy
 import com.tebet.mojual.data.models.enumeration.SortType
+import org.jsoup.helper.StringUtil
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
 
@@ -63,11 +65,22 @@ fun SortType?.toDisplayStatus(): String {
 }
 
 fun String?.fromStatusCodeToDisplayable(): String? {
-    return this?.replace("_", " ")?.toLowerCase()?.capitalize()
-}
-
-fun String?.fromCodeToDisplayable(): String? {
-    return this?.replace("_", " ")
+    return when (this) {
+        OrderStatus.OPEN.name -> Utility.getInstance().getString(R.string.order_status_open)
+        OrderStatus.CLOSED.name -> Utility.getInstance().getString(R.string.order_status_closed)
+        OrderStatus.REJECTED.name -> Utility.getInstance().getString(R.string.order_status_rejected)
+        ContainerOrderState.PAID.name -> Utility.getInstance().getString(R.string.container_status_paid)
+        ContainerOrderState.SALE_CONFIRMED.name -> Utility.getInstance().getString(R.string.container_status_sale_confirmed)
+        ContainerOrderState.FINAL_PRICE_OFFERED.name -> Utility.getInstance().getString(R.string.container_status_final_price_offered)
+        ContainerOrderState.RETURNED.name -> Utility.getInstance().getString(R.string.container_status_returned)
+        ContainerOrderState.SALE_REJECTED.name -> Utility.getInstance().getString(R.string.container_status_sale_rejected)
+        ContainerOrderState.CHECKING.name -> Utility.getInstance().getString(R.string.container_status_checking)
+        ContainerOrderState.SALE_REQUESTED.name -> Utility.getInstance().getString(R.string.container_status_sale_requested)
+        ContainerOrderState.INITIAL_PRICE_REJECTED.name -> Utility.getInstance().getString(R.string.container_status_initial_price_rejected)
+        ContainerOrderState.QUALITY_REJECTED.name -> Utility.getInstance().getString(R.string.container_status_quality_rejected)
+        ContainerOrderState.INITIAL_PRICE_OFFERED.name -> Utility.getInstance().getString(R.string.container_status_initial_price_offered)
+        else -> this?.replace("_", " ")?.toLowerCase()?.capitalize()
+    }
 }
 
 fun Long?.toDisplayLong(): String? {
